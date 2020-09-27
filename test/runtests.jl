9fdc7b06592caa7961024e3e0dad3e9180a4a9ca
Bill <wherrera10@users.noreply.github.com>
2020-09-26 22:16:42 -1000

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,18 @@
+using SuffixTrees, Test
+
+tests = [
+    ("CAAAABAAAABD\$", "AAAAB"),
+    ("GEEKSFORGEEKS\$", "GEEKS"
+    ("AAAAAAAAAA\$", "AAAAAAAAA"),
+    ("ABCDEFG\$", ""),
+    ("ABABABA\$", "ABABA")
+    ("ATCGATCGA\$", "ATCGA"),
+    ("banana\$", "ana"),
+    ("abcpqrabpqpq\$", "ab (or) pq"),
+    ("pqrpqpqabab\$", "ab (or) pq"),
+]
+println("Longest Repeated Substring in:\n")
+for (test, ans) in tests
+    st = SuffixTree(test)
+    @test getlongestrepeatedsubstring(st) == ans
+end
